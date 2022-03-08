d4a44ee8e34c61010964b1d47c67a2d96631867f
ayush808 <94883239+ayush-808@users.noreply.github.com>
2022-03-08 04:58:58 +0000

    done p1 but not getting in least form

--- p2final.c
+++ p2final.c
@@ -1,1 +1,22 @@
 #include<stdio.h>
+
+struct _fraction
+{
+  int num,den;
+}
+
+typedef _fraction Fraction
+
+Fraction input_fraction()
+{
+  Fraction x,y;
+  printf("Enter the numerator and denominator of the fraction: \n");
+  scanf("%d %d",&x.num,&y.den); 
+  return Fraction;
+}
+
+Fraction smallest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{
+  int common_denominator=f1.den*f2.den*f3.den;
+  int num1=f1.num 
+}
